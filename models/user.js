0eb6d357948774a2126cf03cff272a08b490ba64
Mariya <mariyawhitcomb@gmail.com>
2018-06-09 16:24:55 -0400

    created user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    local: {
+        email: String,
+        password: String
+    },
+    books: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Book'
+    }],
+    notes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Note'
+    }]
+})
+module.exports = mongoose.model('User', User)
